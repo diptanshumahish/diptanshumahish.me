3c313755655a383ff9793065728e83ec2408e292
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-26 11:16:05 +0530

    added loading

--- src/components/server/workflow/stepCard.tsx
+++ src/components/server/workflow/stepCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Annie_Use_Your_Telescope } from "next/font/google";
 const annie = Annie_Use_Your_Telescope({ subsets: ["latin"], weight: "400" });
 import Image from "next/image";
+import StepCardImage from "./stepCardImage";
 interface Props {
   stepNum: number;
   stepName: string;
@@ -20,31 +21,25 @@ export default function StepCard({
   stepTime,
 }: Props) {
   return (
-    <div className="w-full flex flex-col gap-4 border border-backgroundDark dark:border-borders-dark lg:border-b-4 border-b-2 rounded-lg md:p-8 p-[14px]">
+    <div className="relative w-full flex flex-col gap-4 border border-backgroundDark dark:border-borders-dark lg:border-b-4 border-b-2 rounded-lg md:p-8 p-[14px]">
       <div className="w-full flex md:flex-row flex-col gap-2 items-start justify-between md:gap-0  md:items-center border-b border-backgroundLight dark:border-borders-dark   pb-5">
-        <div className="bg-backgroundDark dark:bg-backgroundLight lg:text-xl w-fit lg:py-4 lg:px-16 py-2 px-4 text-backgroundLight dark:text-backgroundDark rounded-lg">
+        <div className="bg-backgroundDark dark:bg-backgroundLight lg:text-lg w-fit lg:py-2 lg:px-12 py-2 px-4 text-backgroundLight dark:text-backgroundDark rounded-lg">
           Step {stepNum}.
         </div>
         <div className=" flex flex-col md:items-end text-backgroundDark  bg-sec-theme p-2 px-4 rounded-md border dark:border-borders-dark">
-          <span className="font-bold lg:text-2xl">{stepName}</span>
+          <span className="font-bold lg:text-xl">{stepName}</span>
           <span className={`${annie.className} text-xl`}>{stepSub}</span>
         </div>
       </div>
-      <Image
-        src={stepImage}
-        width={1920}
-        height={200}
-        alt="step 1"
-        className="w-full h-auto dark:invert "
-      />
+      <StepCardImage imageLink={stepImage} />
       <hr className="text-backgroundDark dark:text-borders-dark" />
       <div className="lg:text-lg text-sm text-backgroundDark dark:text-backgroundLight">
         {stepDet}
       </div>
       <div>
         <span className="bg-main-theme px-4 py-2 rounded-lg border flex md:flex-row flex-col ">
-          <span className="font-bold text-xs">Time I spend on this :</span>
-          <span className="text-xs"> {stepTime}</span>
+          <span className="font-bold lg:text-xs">Time I spend on this :</span>
+          <span className="lg:text-xs"> {stepTime}</span>
         </span>
       </div>
     </div>
