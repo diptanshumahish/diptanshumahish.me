6983c83aaaca5f546883d99bc38e3402776fad2b
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-27 01:32:42 +0530

    responseivenss

--- src/components/server/works/workcard.tsx
+++ src/components/server/works/workcard.tsx
@@ -10,7 +10,7 @@ interface Props {
   imageLink: string;
   liveLink: string;
   date: string;
-  type:string
+  type: string;
   colors: string[];
 }
 
@@ -22,43 +22,47 @@ export default function Workcard({
   name,
   technologies,
   colors,
-  type
+  type,
 }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0.5 }}
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.1 }}
-      className=" hidden-scroll overflow-hidden relative snap-center snap-always border border-backgroundDark dark:border-borders-dark lg:p-6 p-2 lg:min-w-[60%] lg:max-w-[60%] min-w-full z-20  rounded-md  flex flex-col lg:gap-4 gap-2 "
+      className=" hidden-scroll overflow-hidden relative snap-center snap-always border border-backgroundDark dark:border-borders-dark lg:p-6 p-2 lg:min-w-[60%] lg:max-w-[60%] min-w-full z-20  rounded-md  flex flex-col justify-between  "
     >
-      <div className="absolute top-0 right-0 p-2 bg-main-theme text-backgroundDark  border-l border-b">
+      <div className="absolute top-0 right-0 p-2 bg-main-theme text-backgroundDark lg:text-md text-xs  border-l border-b">
         {type}
       </div>
-      <h3 className="lg:text-2xl font-bold">{name}</h3>
-      <div>
-        <span className="border  b p-2  rounded-sm lg:text-xl text-xs">
-          {technologies}
-        </span>
-      </div>
-      <div className="w-full flex justify-between items-center">
-        <p className="lg:w-[60%] w-[80%] text-xs lg:text-lg">{details}</p>
-        <div className="w-[20%]">
-          <WorkImage imageLink={imageLink} />
+      <div className="lg:gap-4 gap-3 flex flex-col pb-2 ">
+        <h3 className="lg:text-2xl font-bold w-[70%]">{name}</h3>
+        <div className="">
+          <span className="border  b p-2  rounded-sm lg:text-xl text-xs">
+            {technologies}
+          </span>
+        </div>
+        <div className="w-full flex justify-between lg:items-center lg:flex-row flex-col-reverse lg:gap-0 gap-2 ">
+          <p className="lg:w-[60%]  text-xs lg:text-lg">{details}</p>
+          <div className="lg:w-[20%] w-[30%]">
+            <WorkImage imageLink={imageLink} />
+          </div>
+        </div>
+        <div className="flex gap-2">
+          {colors.map((ele, idx) => {
+            return (
+              <span
+                className={`h-8 w-8 rounded-full  flex border`}
+                style={{ backgroundColor: `${ele}` }}
+                key={idx}
+              />
+            );
+          })}
         </div>
       </div>
-      <div className="flex gap-2">
-        {colors.map((ele, idx) => {
-          return (
-            <span
-              className={`h-8 w-8 rounded-full  flex border`}
-              style={{ backgroundColor: `${ele}` }}
-              key={idx}
-            />
-          );
-        })}
+      <div className="flex flex-col gap-2">
+        <hr />
+        <LinkItems date={date} link={liveLink} />
       </div>
-      <hr />
-      <LinkItems date={date} link={liveLink} />
     </motion.div>
   );
 }
