829100b1dd26cd076df1a7646861648e75686f2e
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-26 11:33:57 +0530

    Added links

--- src/components/server/Header/floatingSocials.tsx
+++ src/components/server/Header/floatingSocials.tsx
@@ -9,16 +9,25 @@ export default function FloatingSocials() {
     <div
       className={`${annie.className} transition-colors z-50 bg-backgroundLight dark:bg-backgroundDark dark:text-backgroundLight  fixed lg:top-28 top-14 right-2 lg:right-10 flex border py-2 px-4 border-b-2 dark:border-borders-dark lg:text-[20px] text-[12px] rounded-md gap-6 `}
     >
-      <Link href="" className="flex flex-col items-center ">
+      <Link
+        href="https://www.instagram.com/_diptanshuu_/"
+        className="flex flex-col items-center "
+      >
         <InstagramLogo />
         Insta
       </Link>
 
-      <Link href="" className="flex flex-col items-center">
+      <Link
+        href="https://github.com/diptanshumahish"
+        className="flex flex-col items-center"
+      >
         <GithubLogo />
         Github
       </Link>
-      <Link href="" className="flex flex-col items-center">
+      <Link
+        href="https://medium.com/@uiuxwithdiptanshu"
+        className="flex flex-col items-center"
+      >
         <MediumLogo />
         Medium
       </Link>
