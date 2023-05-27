729855cf5b9da349ef6c0a1faa22026e62cf026c
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-27 19:37:55 +0530

    fixed overflow

--- src/components/server/Education/Education.tsx
+++ src/components/server/Education/Education.tsx
@@ -5,7 +5,7 @@ const annie = Annie_Use_Your_Telescope({ subsets: ["latin"], weight: "400" });
 
 export default function Education() {
   return (
-    <div className="   w-full lg:p-10 p-5 text-backgroundDark dark:text-backgroundLight">
+    <div className="overflow-hidden   w-full lg:p-10 p-5 text-backgroundDark dark:text-backgroundLight">
       <div className="w-[40%] flex flex-col ">
         <span className="w-[20%] lg:hidden h-[2px] rounded-md bg-backgroundDark dark:bg-backgroundLight my-2"></span>
         <span>And about my </span>
