aafb8214cf6e113b3d459502aa7f0faf77a28e7e
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-28 01:30:26 +0530

    added hiding

--- src/api-fetch/providers.tsx
+++ src/api-fetch/providers.tsx
@@ -5,11 +5,21 @@ import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 export default function Providers({ children }: React.PropsWithChildren) {
+  var tempScroll: number = window.scrollY;
   useEffect(() => {
     const theme = localStorage.getItem("themeMode");
     if (theme === "true") {
       document.body.classList.add("dark");
     }
+    window.onscroll = () => {
+      var currentScroll = window.scrollY;
+      if (tempScroll > currentScroll) {
+        document.getElementById("flt")?.classList.remove("hidden");
+      } else {
+        document.getElementById("flt")?.classList.add("hidden");
+      }
+      tempScroll = currentScroll;
+    };
   }, []);
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
