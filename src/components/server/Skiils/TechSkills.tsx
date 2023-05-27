7a8f92139852bd00a78454bdeb1f005569d88b84
diptanshumahish <diptanshumahish2016@gmail.com>
2023-05-27 12:01:20 +0530

    animationsss

--- src/components/server/Skiils/TechSkills.tsx
+++ src/components/server/Skiils/TechSkills.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 interface Props {
   skills: string[];
   isNonTech?: boolean;
@@ -9,14 +11,17 @@ export default function TechSkills({ skills, isNonTech = false }: Props) {
     <div className="py-2 z-10 tracking-widest lg:text-md text-xs flex gap-3 w-full flex-wrap">
       {skills.map((ele, idx) => {
         return (
-          <span
+          <motion.span
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: idx * 0.09 }}
             key={idx}
             className={` ${
               isNonTech ? "bg-sec-theme" : "bg-main-theme"
             } border dark p-1 px-2 rounded-md text-backgroundDark z-10`}
           >
             {ele}
-          </span>
+          </motion.span>
         );
       })}
     </div>
